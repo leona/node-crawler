aaa0f880fdd390d7729439e09ef9efe9e4f6f821
Leon Harvey <leonharvey@live.com>
2015-07-10 09:49:35 +0000

    0.0.3

--- app.js
+++ app.js
@@ -7,21 +7,49 @@ var crawler = {
     site_store: [],
     
     initiate: function(max_procs, url, depth) {
+        
         this.fetchBody(url, function(body) {
-            var this.site_store = crawler.parseUrl(body);
-            var iter_count  = this.site_store.length;
-            
-            while (crawler.site_store < depth) {
+            this.site_store = crawler.parseUrl(body);
+            this.site_store = crawler.cleanArray(this.site_store);
+            var iter_count  = 0;
+   
+            while (this.site_store.length < depth) {
                 var work_array = [];
+                var iter_stop;
                 
+                if (this.site_store.length > iter_count + max_procs) {
+                    iter_stop = iter_count + max_procs;
+                } else {
+                    iter_stop = this.site_store.length;
+                }
+                
+                work_array = this.site_store.slice(iter_count, iter_stop);
+         
                 async.map(work_array, crawler.thread, function(err, results){
-                    console.log('finished');
+                    console.log(err);
                 });
+                
+                //for(i = 0;i < work_array.length;i++) {
+                    fs.readFile(crawler.datPath(), 'utf8', function (err, data) {
+                      if (err) {
+                        return console.log('No urls parsed: ' + err);
+                      }
+                      this.site_store.push(data.split("\n"));
+                    });
+                
+                //}
+                
+                break;
             }
         });
     },
-    thread: function() {
-        
+    thread: function(item, callback) {
+        crawler.fetchBody(item, function(body) {
+            crawler.site_store = crawler.parseUrl(body);
+         
+            if (typeof crawler.site_store != "undefined" && crawler.site_store != null && crawler.site_store.length > 0)
+                fs.writeFile(crawler.datPath(item), crawler.site_store.join("\n")); 
+        })
     },
     parseUrl: function(body) {
         if (typeof body !== 'undefined')
@@ -30,7 +58,7 @@ var crawler = {
     fetchBody: function(url, callback) {
         request.get(url).on('response', function(response) {
             var body = '';
-            
+         
             response.on('data', function(chunk) {
                 body += chunk;
             });
@@ -41,27 +69,13 @@ var crawler = {
         });
     },
     datPath: function(url) {
+        return __dirname + '/store/dat_';
         return __dirname + '/store/dat_' + md5(url);
+    },
+    cleanArray: function(arr) {
+        return arr.filter(function(value, index, self) { 
+            return self.indexOf(value) === index;
+        });
     }
 }
-crawler.initiate(200, 'http://leonharvey.com/', 20);
-/*
-async.map(test, proc.fetchBody, function(err, results){
-    console.log('finished');
-});
-*/
-/* 
-app.get('/', function(req, res) {
-   
-
-    async.parallel([
-        proc.thread,
-        proc.thread
-    ], function(err, results) {
-        //console.log(err, results);
-        //console.log(results);
-    });
-    
-    res.send('Hello World!');
-});
-*/
+crawler.initiate(200, 'https://news.layervault.com/', 300);
